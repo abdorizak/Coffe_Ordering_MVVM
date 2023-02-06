4728a0adb97fd029e122b03caf46487080b732b9
Abdorizak Abdalla <cabdirizaaqyare12@gmail.com>
2023-02-06 21:35:22 +0300

    TextField

--- Coffee Ordering/Views/Custom Views/TextField/TextField.swift
+++ Coffee Ordering/Views/Custom Views/TextField/TextField.swift
@@ -6,3 +6,37 @@
 //
 
 import Foundation
+import UIKit
+
+final class TextField: UITextField {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        config()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func config(){
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        textContentType = .username
+        
+        layer.cornerRadius = 5
+        
+        textColor           = .label
+        tintColor           = .label
+        
+        textAlignment       = .left
+        font                = UIFont.systemFont(ofSize: 17)
+        minimumFontSize     = 16
+        
+        backgroundColor     = .clear
+        autocorrectionType  = .no
+        returnKeyType       = .next
+        clearButtonMode     = .whileEditing
+        placeholder         = "Enter Username"
+    }
+}
